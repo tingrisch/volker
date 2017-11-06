7ab65fa1d05598c031fbe5f8b8d4233c473e7080
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-11-06 22:48:37 +0100

    configuration stuff

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
-import styled from "styled-components"
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 
-import Header from "./app/components/header/Header";
-import Sidebar from "./app/components/sidebar/Sidebar";
-import Main from "./app/components/main/Main";
+import Header from './app/components/header/Header';
+import Sidebar from './app/components/sidebar/Sidebar';
+import Main from './app/components/main/Main';
 
 const Wrapper = styled.div`
   background: #fdf1c1;
@@ -12,13 +12,9 @@ const Wrapper = styled.div`
 
 class App extends Component {
   render() {
-    return (
-      <Wrapper>
-        
-      </Wrapper>
-    );
+    return <Wrapper />;
   }
 }
 
-const root = document.querySelector("#app");
+const root = document.querySelector('#app');
 ReactDOM.render(<App />, root);
