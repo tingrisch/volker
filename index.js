ba50c54ae3bed0238b6a3087221b622f029e753d
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-11-06 17:28:44 +0100

    test for Header

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import styled from "styled-components"
 
 import Header from "./app/components/header/Header";
+import Sidebar from "./app/components/sidebar/Sidebar";
+import Main from "./app/components/main/Main";
+
+const Wrapper = styled.div`
+  background: #fdf1c1;
+`;
 
 class App extends Component {
   render() {
     return (
-      <div>Welcome to React!</div>
+      <Wrapper>
+        
+      </Wrapper>
     );
   }
 }
---
+++ test/Header.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Header from '../app/components/header/Header';
+
+describe('<Header />', () => {
+
+  test('renders', () => {
+    //const HeaderComponent = shallow(<Header />);
+    
+  });
+
+});
