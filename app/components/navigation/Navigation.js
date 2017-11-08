e4151dd36d6bf17d3eb76bf0fe6dac807ac36a26
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-11-08 23:34:52 +0100

    started to build the menu

---
+++ app/components/navigation/Navigation.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const MainNavigation = styled.nav``;
+
+const Menu = styled.ul`
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuElement = styled.li`
+  display: block;
+  list-style-type: none;
+`;
+
+const MenuLink = styled.a`
+  display: flex;
+  text-align: center;
+  text-decoration: none;
+  color: #777;
+  padding: 1rem;
+
+  &:hover {
+    background: #777;
+    color: #fff;
+  }
+`;
+
+// https://codepen.io/joslex/pen/adeJeV
+
+const Navigation = () => {
+  return (
+    <div>
+      <MainNavigation
+        <Menu>
+          <MenuElement>
+            <MenuLink>Über mich</MenuLink>
+          </MenuElement>
+          <MenuElement>
+            <MenuLink>Produkte</MenuLink>
+          </MenuElement>
+          <MenuElement>
+            <MenuLink>Cooles Zeug</MenuLink>
+          </MenuElement>
+        </Menu>
+      </MainNavigation>
+    </div>
+  );
+};
+
+export default Navigation;
--- app/styles/Theme.js
+++ app/styles/Theme.js
@@ -1,20 +1,23 @@
 const colors = {
   transparent: 'transparent',
   dark: '#222222',
+  white: '#ffffff'
 };
-
 
 const fontSizes = {
   xtraSmall: '9px',
   small: '10px',
   medium: '13px',
   large: '16px',
-  xtraLarge: '18px',
+  xtraLarge: '18px'
 };
 
 const Theme = {
   ...colors,
   ...fontSizes,
+  menu: {
+    border: '#CB6206'
+  }
 };
 
 export default Theme;
