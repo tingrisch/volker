2c821f3863e8db85433c008ce4ab7d5ae39fb97e
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-11-20 16:06:08 +0100

    404 error in webpack server fixed

--- app/components/main/Main.js
+++ app/components/main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 const Wrapper = styled.div`
   background: rgba(255, 255, 255, 0.8);
@@ -8,6 +8,10 @@ const Wrapper = styled.div`
   box-shadow: 3px 3px 4px 0px rgba(0, 0, 0, 0.25);
   float: right;
 `;
+
+const Home = () => {
+  return <div>Home</div>;
+};
 
 const About = () => {
   return <div>Über mich</div>;
@@ -21,8 +25,11 @@ const Main = () => {
   return (
     <Wrapper>
       Test dskfj sdjfskdl jflsdf
-      <Route path="/ueber-mich/" component={About} />
-      <Route path="/kontakt/" component={Contact} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/ueber-mich/" component={About} />
+        <Route path="/kontakt/" component={Contact} />
+      </Switch>
     </Wrapper>
   );
 };
