4e0ded8c877aacb2c50c5f3cdbf82c6fcda78d04
Tiemo Ingrisch <tiemo.ingrisch@jameda.de>
2017-11-24 16:19:47 +0100

    image

--- app/components/home/Home.js
+++ app/components/home/Home.js
@@ -2,10 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import H1 from '../ui/headline/H1';
+//import homeImage from './ruhe_frieden.jpg';
 
 const Home = () => {
   return (
-    <H1>Willkommen auf meinen Seiten</H1>
+    <div>
+      <H1>Willkommen auf meinen Seiten</H1>
+    </div>
   );
 };
 
